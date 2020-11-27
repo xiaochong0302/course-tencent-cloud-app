0aa90b0433dc094bdd56f2f0875f8e3f53da401f
xiaochong0302 <xiaochong0302@gmail.com>
2020-11-27 17:49:14 +0800

    去除manifest.json h5部分无用配置

---
+++ common/config.sample.js
@@ -0,0 +1,11 @@
+/**
+ * NOTE:尾部不要加"/"
+ */
+export const apiBaseUrl = 'https://ctc.koogua.com/api'
+
+export const appInfo = {
+	name: '酷瓜云课堂',
+	alias: 'ctc-app',
+	link: 'https://gitee.com/koogua/course-tencent-cloud-app',
+	version: '1.0.0'
+}
